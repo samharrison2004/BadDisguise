9fd815632d07ed89761680380ea30855892d5cfe
Sam Harrison <harrison.2004@outlook.com>
2024-12-29 21:00:20 +0000

    Added subprocess call

--- BadDisguise.py
+++ BadDisguise.py
@@ -7,9 +7,24 @@ import argparse
 ## Required to change the MAC address of the device
 import os
 
+## Required to execute 'ping' command in the shell
+import subprocess
+
+## Required to 
+import platform
+
 ## Scapy is required to interact with ARP requests
 from scapy.all import *
 
+ping_target = "google.com"
+
+def is_connected(ping_target):
+    if platform.system().lower() == "windows":
+        argument = "-n"
+    else:
+        argument = "-c"
+    command = ["ping", argument, "1", ping_target]
+    return subprocess.call(command) == 1 ## Will return "True" if the target site can be reached
 
 parser = argparse.ArgumentParser()
 
